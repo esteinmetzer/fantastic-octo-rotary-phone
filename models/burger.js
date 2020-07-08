f8276e7d0de8d17a76aba43b54a206df4d77a412
esteinmetzer <esteinmetzeruw@gmail.com>
2020-07-07 19:55:17 -0700

    final commit

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var burger = {
+    all: function (cb) {
+        orm.selectAll("burgers" , function (res) {
+            cb(res);
+        });
+    },
+    create: function (val, cb) {
+        orm.insertOne("burgers", "burger_name", val, function (res) {
+            cb(res);
+        });
+    },
+    update: function(colVal, condition, cb) {
+        orm.updateOne("burgers", colVal, condition, function(res) {
+          cb(res);
+        });
+      },
+};
+
+// Export the database functions for the controller (burgers_controller.js).
+module.exports = burger;
---
+++ public/assets/javascript/main.js
@@ -0,0 +1,43 @@
+// Function to house both the on click functions -- Waits for the DOM to be loaded
+$(function () {
+  // On CLick function of the buttons next to the burger names
+  $(".changeDevoured").on("click", function (event) {
+    // Variables needed
+    var id = $(this).data("id");
+    var devouredstate = $(this).data("devouredstate");
+    var newDevouredState = {
+      devoured: devouredstate
+    };
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevouredState
+    }).then(
+      function () {
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+  // On Click function that creates a burger
+  $("#makeBurger").on("click", function (event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+    // Variable for the newBurger
+    var newBurger = {
+      name: $("#newBurgerName").val().trim(),
+      devoured: false
+    };
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function () {
+        console.log("created New Burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    )
+  })
+});
